4e582abb71e2f8b4e467fbcfa66ea296a683ca15
Agnibha Ray <64902131+AgnibhaRay@users.noreply.github.com>
2023-08-11 21:13:40 +0530

    Add files via upload

---
+++ src/lib/Progress.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import newMoon from "../lib/assets/new-moon.svg";
+
+    function calculateProgress(startDate: Date, endDate: Date): number {
+        const currentDate = new Date();
+        const totalDays = Math.ceil(
+            (endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24)
+        );
+        const elapsedDays = Math.ceil(
+            (currentDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24)
+        );
+        return (elapsedDays / totalDays) * 100;
+    }
+
+    const startDate = new Date('2023-08-01');
+    const endDate = new Date('2023-08-30');
+    const progress = calculateProgress(startDate, endDate);
+</script>
+
+<div class="progress-container">
+    <div class="bubble" style="margin-left: {progress}%">
+        <img src="{newMoon}" width="32px" height="32px" alt="moon">
+    </div>
+    <div class="progress-done">
+        <div class="progress-not-done" style="width: {progress}%"></div>
+    </div>
+    <div class="timeline">
+        <span>01 Aug</span>
+        <span>30 Aug</span>
+    </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import logo from "../lib/assets/logo.png"
+    import Progress from "$lib/Progress.svelte";
+</script>
+
+<main>
+    <img src={logo} width="190" height="190" alt="solarize-logo">
+    <span class="title">Project Solarize</span>
+    <Progress/>
+    <span class="body">One Step forward towards a greener earth 🌏</span>
+    <span class="body">Work in Progress 👷‍♂️👷‍♀️</span>
+</main>
