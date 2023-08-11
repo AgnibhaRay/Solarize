02374f1dce17f50d1568b15593e3658bb58b8018
Agnibha Ray <64902131+AgnibhaRay@users.noreply.github.com>
2023-08-11 22:24:43 +0530

    Svelte Commit
    The Site was restructured using Svelte Framework

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import logo from "../lib/assets/logo.png"
+    import ttv from "../lib/assets/ttv-lg.png"
     import Progress from "$lib/Progress.svelte";
 </script>
 
+<img src={ttv} width="70" height="70" alt="ttv-logo" style="position: absolute; margin: 16px">
 <main>
     <img src={logo} width="190" height="190" alt="solarize-logo">
     <span class="title">Project Solarize</span>
     <Progress/>
-    <span class="body">One Step forward towards a greener earth 🌏</span>
-    <span class="body">Work in Progress 👷‍♂️👷‍♀️</span>
+    <span>One Step forward towards a greener earth 🌏</span>
+    <span>Work in Progress 👷‍♂️👷‍♀️</span>
 </main>
